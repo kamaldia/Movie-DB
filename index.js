c174222d02af2f3c00543fffbed6d99860c5d369
Kamal <kamal_dia@hotmail.com>
2023-09-24 23:23:35 +0300

    step 6

--- index.js
+++ index.js
@@ -33,60 +33,85 @@ app.get("/search", (req, res) => {
   }
 });
 
-app.get("/movies", (req, res) => {
-  const movie_add = req.query.add;
-  const movie_get = req.query.get;
-  const movie_edit = req.query.edit;
-  const movie_delete = req.query.delete;
-  if (movie_add !== undefined) { //execute if there is add query in the url
-    movies.push(movie_add); //adds input to movies array
-    res.status(200);
-    res.send(`status:200, message:"ok", data added: ${movie_add}`);
-  }
-  else if (movie_get !== undefined){
-    // const movie_found = movies.find(movie => movie.title === movie_get); // movies.find will return undefined if there is no movie with a title matching the input
-    // if (movie_found) { // if movie in query exists in the array, it will execute
-    //   res.status(200);
-    //   res.send(`status:200, message:"ok", data found: ${movie_get}`); // requested movie is shown with its properties
-    // }
-    // else { // if entered movie is not in array
-    //   res.status(200);
-    //   res.send(`status:200, message:"ok", data: ${movie_get} is not found`);
-    // }
-    const movie_list = movies.map((movie) => movie.title + " "); //mapping over list, taking the title from each object and adding a space to seperate them
-    console.log(movie_list);
-    res.status(200);
-    res.send(`status:200, message:"ok", data: ${movie_list}`); //just read the instructions for step 5
-  }
-  else if (movie_edit !== undefined) { // if edit query exists
-    const movie_to_edit = (movie_edit.trim()).split(" "); // made input an array, assuming that the string entered contains "value to edit" "value to replace it" seperated by whitespace, and trimmed to avoid conflict
-    const movie_found = movies.find(movie => movie.title === movie_to_edit[0]);
-    console.log(movie_to_edit);
-    if (movie_found) { // if movie in query exists in the array, it will execute
-      movies[movie_to_edit[0]].title = movie_to_edit[1];
-      res.status(200);
-      res.send(`status:200, message:"ok", data edited: ${movies[movie_to_edit[0]].title} to ${movie_to_edit[1]}`);
-    }
-    else { // if entered movie is not in array
-      res.status(200);
-      res.send(`status:200, message:"ok", data: ${movie_to_edit[0]} is not found`);
-    }
-  }
-  else if (movie_delete !== undefined){
-    if (movies.includes(movie_delete)) { // if movie in query exists in the array, it will execute
-      res.status(200);
-      res.send(`status:200, message:"ok", data deleted: ${movies.pop(movies[movie_delete])}`); // requested movie is shown with its properties
-    }
-    else { // if entered movie is not in array
-      res.status(200);
-      res.send(`status:200, message:"ok", data: ${movie_delete} is not found`);
-    }
-  }
-  else {
-    res.status(500);
-    res.send("status:500, error:true, message:\"you have to provide a movie query");
-  }
-});
+/* Step 6 ------------------------------------------------------------- */
+
+app.get("/movies/get/by-date", (req, res) => {
+  const movies_by_date = movies.sort((movie_1, movie_2) => (movie_1.year > movie_2.year) ? 1 : (movie_1.year < movie_2.year) ? -1 : 0); //sorting with the help of comparing function
+  // console.log(movies_by_date);
+  res.status(200);
+  res.send(`status:200, message:"ok", data: ${movies_by_date.map((movie) => " " + movie.title)}`)
+})
+
+app.get("/movies/get/by-rating", (req, res) => {
+  const movies_by_rating = movies.sort((movie_1, movie_2) => (movie_1.rating > movie_2.rating) ? -1 : (movie_1.rating < movie_2.rating) ? 1 : 0); //sorting with the help of comparing function, this time we put the values opposite to get descending order
+  // console.log(movies_by_rating);
+  res.status(200);
+  res.send(`status:200, message:"ok", data: ${movies_by_rating.map((movie) => " " + movie.title)}`)
+})
+
+app.get("/movies/get/by-title", (req, res) => {
+  const movies_by_title = movies.sort((movie_1, movie_2) => (movie_1.title[0] > movie_2.title[0]) ? 1 : (movie_1.title[0] < movie_2.title[0]) ? -1 : 0); //sorting with the help of comparing function
+  // console.log(movies_by_title);
+  res.status(200);
+  res.send(`status:200, message:"ok", data: ${movies_by_title.map((movie) => " " + movie.title)}`)
+})
+
+/* ---------------------------------------------------------------------- */
+
+// app.get("/movies", (req, res) => {
+//   const movie_add = req.query.add;
+//   const movie_get = req.query.get;
+//   const movie_edit = req.query.edit;
+//   const movie_delete = req.query.delete;
+//   if (movie_add !== undefined) { //execute if there is add query in the url
+//     movies.push(movie_add); //adds input to movies array
+//     res.status(200);
+//     res.send(`status:200, message:"ok", data added: ${movie_add}`);
+//   }
+//   else if (movie_get !== undefined){
+//     // const movie_found = movies.find(movie => movie.title === movie_get); // movies.find will return undefined if there is no movie with a title matching the input
+//     // if (movie_found) { // if movie in query exists in the array, it will execute
+//     //   res.status(200);
+//     //   res.send(`status:200, message:"ok", data found: ${movie_get}`); // requested movie is shown with its properties
+//     // }
+//     // else { // if entered movie is not in array
+//     //   res.status(200);
+//     //   res.send(`status:200, message:"ok", data: ${movie_get} is not found`);
+//     // }
+//     const movie_list = movies.map((movie) => movie.title + " "); //mapping over list, taking the title from each object and adding a space to seperate them
+//     console.log(movie_list);
+//     res.status(200);
+//     res.send(`status:200, message:"ok", data: ${movie_list}`); //just read the instructions for step 5
+//   }
+//   else if (movie_edit !== undefined) { // if edit query exists
+//     const movie_to_edit = (movie_edit.trim()).split(" "); // made input an array, assuming that the string entered contains "value to edit" "value to replace it" seperated by whitespace, and trimmed to avoid conflict
+//     const movie_found = movies.find(movie => movie.title === movie_to_edit[0]);
+//     console.log(movie_to_edit);
+//     if (movie_found) { // if movie in query exists in the array, it will execute
+//       movies[movie_to_edit[0]].title = movie_to_edit[1];
+//       res.status(200);
+//       res.send(`status:200, message:"ok", data edited: ${movies[movie_to_edit[0]].title} to ${movie_to_edit[1]}`);
+//     }
+//     else { // if entered movie is not in array
+//       res.status(200);
+//       res.send(`status:200, message:"ok", data: ${movie_to_edit[0]} is not found`);
+//     }
+//   }
+//   else if (movie_delete !== undefined){
+//     if (movies.includes(movie_delete)) { // if movie in query exists in the array, it will execute
+//       res.status(200);
+//       res.send(`status:200, message:"ok", data deleted: ${movies.pop(movies[movie_delete])}`); // requested movie is shown with its properties
+//     }
+//     else { // if entered movie is not in array
+//       res.status(200);
+//       res.send(`status:200, message:"ok", data: ${movie_delete} is not found`);
+//     }
+//   }
+//   else {
+//     res.status(500);
+//     res.send("status:500, error:true, message:\"you have to provide a movie query");
+//   }
+// });
 
 app.listen(the_port, (error) => { // listener to know if server is running
   if (!error)
